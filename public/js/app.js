d9ac91a31475e00b87efb81abc09643353bd68fb
Ryan-Auth0 <73363667+Ryan-Auth0@users.noreply.github.com>
2020-11-05 18:15:55 -0500

    auth0 variable 6

--- public/js/app.js
+++ public/js/app.js
@@ -172,9 +172,35 @@ window.onload = async () => {
     })
   });
   
+  document.getElementById('btn-login').addEventListener('click', function() {
+    webAuth.authorize();
+  });
 
+  document.getElementById('btn-logout').addEventListener('click', function() {
+    logout();
+  });
 
-
+  webAuth.parseHash({ hash: window.location.hash }, (err, authResult) => {
+    if (err) {
+      return console.error(err);
+    }
+    if (authResult) {
+      webAuth.client.userInfo(authResult.accessToken, (err, profile) => {
+        if (err) {
+          // Remove expired token (if any)
+          localStorage.removeItem('token');
+          // Remove expired profile (if any)
+          localStorage.removeItem('profile');
+          return alert('There was an error getting the profile: ' + err.message);
+        } else {
+          localStorage.setItem('token', authResult.accessToken);
+          localStorage.setItem('profile', JSON.stringify(profile));
+          showUserProfile(profile);
+        }
+        window.location.hash = "";
+      });
+    }
+  });
 
   var checkAuth = function() {
     var token = localStorage.getItem('token');
